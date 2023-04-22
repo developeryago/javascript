e1c659e8007e37b90ebace96c72d9c835b3bb82c
developeryago <contato.yagomachado@outlook.com>
2023-04-22 15:28:10 -0300

    finish

---
+++ exercicios/ex01/script.js
@@ -0,0 +1,19 @@
+function Carregar(){
+    var agora = new Date()
+    var hora = agora.getHours()
+    var mensagem = document.getElementById("msg")
+    var img = document.getElementById("foto")
+    var fundo = document.getElementById("fundo")
+    if (hora > 6 && hora < 12){
+        mensagem.innerHTML = `Bom dia! Agora são ${hora}hrs`
+        img.src = 'manha.jpg'
+    } else if (hora > 12 && hora < 18){
+        mensagem.innerHTML = `Boa tarde! Agora são ${hora}hrs`
+        fundo.style.backgroundColor = "rgb(94, 76, 50)"
+        img.src = 'tarde.jpg'
+    } else if (hora > 18){
+        mensagem.innerHTML = `Boa noite! Agora são ${hora}hrs`
+        fundo.style.backgroundColor = "rgb(75, 75, 75)"
+        img.src = 'noite.jpg'
+    } 
+}
