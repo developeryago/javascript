2222ba3ec695f5e27157fc6bb7ecba483a766e74
developeryago <contato.yagomachado@outlook.com>
2023-04-24 08:35:20 -0300

    bug fix

--- exercicios/ex01/script.js
+++ exercicios/ex01/script.js
@@ -4,14 +4,14 @@ function Carregar(){
     var mensagem = document.getElementById("msg")
     var img = document.getElementById("foto")
     var fundo = document.getElementById("fundo")
-    if (hora > 6 && hora < 12){
+    if (hora >= 4 && hora < 12){
         mensagem.innerHTML = `Bom dia! Agora são ${hora}hrs`
         img.src = 'manha.jpg'
-    } else if (hora > 12 && hora < 18){
+    } else if (hora >= 12 && hora < 18){
         mensagem.innerHTML = `Boa tarde! Agora são ${hora}hrs`
         fundo.style.backgroundColor = "rgb(94, 76, 50)"
         img.src = 'tarde.jpg'
-    } else if (hora > 18){
+    } else{
         mensagem.innerHTML = `Boa noite! Agora são ${hora}hrs`
         fundo.style.backgroundColor = "black"
         img.src = 'noite.jpg'
